aluamorim/openwrt

commit 00a5c7cdbc3d45210c98980e501b1c49ef50588e

[package] madwifi: make wep-open the default
git-svn-id: svn://svn.openwrt.org/openwrt/trunk@23660 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/madwifi/files/lib/wifi/madwifi.sh
@@ -217,8 +217,8 @@ enable_atheros() {
 		case "$enc" in
 			wep*)
 				case "$enc" in
-					*open*) iwpriv "$ifname" authmode 1;;
-					*)      iwpriv "$ifname" authmode 2;;
+					*shared*) iwpriv "$ifname" authmode 2;;
+					*)        iwpriv "$ifname" authmode 1;;
 				;;
 				for idx in 1 2 3 4; do
 					config_get key "$vif" "key${idx}"

commit 02bbce2a18a54513c2d5077c8663100250b4fcd6

madwifi: fix syntax error introduced by r23660
SVN-Revision: 23679

package/madwifi/files/lib/wifi/madwifi.sh
@@ -219,7 +219,7 @@ enable_atheros() {
 				case "$enc" in
 					*shared*) iwpriv "$ifname" authmode 2;;
 					*)        iwpriv "$ifname" authmode 1;;
-				;;
+				esac
 				for idx in 1 2 3 4; do
 					config_get key "$vif" "key${idx}"
 					iwconfig "$ifname" enc "[$idx]" "${key:-off}"