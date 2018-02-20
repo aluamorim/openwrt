aluamorim/openwrt

commit c60a21532bc903ab86d971b478752152d4476ef8

ramips: Add support for the GnuBee Personal Cloud Two
The GnuBee Personal Cloud Two crowdfunded on https://www.crowdsupply.com
It is a low-cost, low-power, network-attached storage device.

Specifications:

- SoC: MediaTek MT7621AT
- RAM: DDR3 512 MB
- Flash: 32 MB
- Six SATA ports for 3.5" Drives
- One SDcard
- One USB 3.0
- Two USB 2.0
- Gigabit Ethernet: Three Ports
- UART 3.5mm Audio Jack or 3 pin header - 57600 8N1
- Three GPIOs available on a pin header

Flash instructions:

The GnuBee Personal Cloud Two ships with libreCMC installed.
libreCMC is a Free Software Foundation approved fork of LEDE/OpenWrt.
As such one can upgrade using the webinterface or sysupgrade.

Das U-Boot has multiple options for recovery or updates including :

- USB
- http
- tftp

Errata:
- While there are three ethernet ports, the third requires support for
  the second GMAC. This will come in kernel 4.14.
- The first hard drive slot has a clearance issue with the two fan
  headers. Workaround is to pull the headers out and connect the pins to
  jumper wires.
- Using this device as a NAS is problematic with the 4.9 kernel as many
  /dev/sdX reads throw silent errors. The current theory behind this is
  some kind of unhandled DMA mapping error in the kernel. This is not an
  issue with kernel 4.4.

Signed-off-by: L. D. Pinney <ldpinney@gmail.com>
Signed-off-by: Rosen Penev <rosenp@gmail.com>

target/linux/ramips/base-files/etc/board.d/01_leds
@@ -191,6 +191,12 @@ gb-pc1)
 	ucidef_set_led_switch "lan1" "lan1" "$boardname:green:lan1" "switch0" "0x01"
 	ucidef_set_led_switch "lan2" "lan2" "$boardname:green:lan2" "switch0" "0x10"
 	;;
+gb-pc2)
+	ucidef_set_led_switch "lan" "lan" "$board:green:lan" "switch0" "0x01"
+	ucidef_set_led_switch "wan" "wan" "$board:green:wan" "switch0" "0x10"
+	ucidef_set_led_netdev "eth1-g" "eth1-g" "$board:green:eth1-g" "eth1"
+	ucidef_set_led_netdev "eth1-y" "eth1-y" "$board:green:eth1-y" "eth1"
+	;;
 gl-mt300a|\
 gl-mt300n|\
 gl-mt750)

target/linux/ramips/base-files/etc/board.d/02_network
@@ -206,6 +206,10 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0:lan" "4:lan" "6@eth0"
 		;;
+	gb-pc2)
+		ucidef_add_switch "switch0" \
+			"4:lan" "0:wan" "6@eth0"
+		;;
 	gl-mt300n-v2)
 		ucidef_add_switch "switch0" \
 			"1:lan" "0:wan" "6@eth0"

target/linux/ramips/base-files/etc/diag.sh
@@ -91,6 +91,7 @@ get_status_led() {
 	dir-620-d1|\
 	dwr-512-b|\
 	gb-pc1|\
+	gb-pc2|\
 	hpm|\
 	hw550-3g|\
 	mac1200rv2|\

target/linux/ramips/base-files/lib/ramips.sh
@@ -205,6 +205,9 @@ ramips_board_detect() {
 	*"GB-PC1")
 		name="gb-pc1"
 		;;
+	*"GB-PC2")
+		name="gb-pc2"
+		;;
 	*"GL-MT300A")
 		name="gl-mt300a"
 		;;

target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -61,6 +61,7 @@ platform_check_image() {
 	fonera20n|\
 	freestation5|\
 	gb-pc1|\
+	gb-pc2|\
 	gl-mt300a|\
 	gl-mt300n|\
 	gl-mt750|\

target/linux/ramips/dts/GB-PC2.dts
@@ -1,0 +1,137 @@
+/dts-v1/;
+
+#include "mt7621.dtsi"
+
+#include <dt-bindings/gpio/gpio.h>
+#include <dt-bindings/input/input.h>
+
+/ {
+	compatible = "gnubee,gb-pc2", "mediatek,mt7621-soc";
+	model = "GB-PC2";
+
+	memory@0 {
+		device_type = "memory";
+		reg = <0x0 0x1c000000>, <0x20000000 0x4000000>;
+	};
+
+	chosen {
+		bootargs = "console=ttyS0,57600";
+	};
+
+	palmbus: palmbus@1E000000 {
+		i2c@900 {
+			status = "okay";
+		};
+	};
+
+	gpio-keys-polled {
+		compatible = "gpio-keys-polled";
+		#address-cells = <1>;
+		#size-cells = <0>;
+		poll-interval = <20>;
+
+		reset {
+			label = "reset";
+			gpios = <&gpio0 18 GPIO_ACTIVE_HIGH>;
+			linux,code = <KEY_RESTART>;
+		};
+	};
+
+	gpio-leds {
+		compatible = "gpio-leds";
+
+		system {
+			label = "gb-pc2:green:system";
+			gpios = <&gpio0 6 GPIO_ACTIVE_LOW>;
+		};
+
+		status {
+			label = "gb-pc2:green:status";
+			gpios = <&gpio0 8 GPIO_ACTIVE_LOW>;
+		};
+
+		lan {
+			label = "gb-pc2:green:lan";
+			gpios = <&gpio0 24 GPIO_ACTIVE_LOW>;
+		};
+
+		wan {
+			label = "gb-pc2:green:wan";
+			gpios = <&gpio0 25 GPIO_ACTIVE_LOW>;
+		};
+
+		eth1-y {
+			label = "gb-pc2:yellow:eth1-y";
+			gpios = <&gpio0 13 GPIO_ACTIVE_LOW>;
+		};
+
+		eth1-g {
+			label = "gb-pc2:green:eth1-g";
+			gpios = <&gpio0 15 GPIO_ACTIVE_LOW>;
+		};
+
+	};
+};
+
+&sdhci {
+	status = "okay";
+};
+
+&spi0 {
+	status = "okay";
+
+	m25p80@0 {
+		#address-cells = <1>;
+		#size-cells = <1>;
+		compatible = "jedec,spi-nor";
+		reg = <0>;
+		spi-max-frequency = <10000000>;
+		m25p,chunked-io = <32>;
+
+		partition@0 {
+			label = "u-boot";
+			reg = <0x0 0x30000>;
+			read-only;
+		};
+
+		partition@30000 {
+			label = "u-boot-env";
+			reg = <0x30000 0x10000>;
+			read-only;
+		};
+
+		factory: partition@40000 {
+			label = "factory";
+			reg = <0x40000 0x10000>;
+			read-only;
+		};
+
+		partition@50000 {
+			label = "firmware";
+			reg = <0x50000 0x1fb0000>;
+		};
+	};
+};
+
+&cpuclock {
+			compatible = "fixed-clock";
+			clock-frequency = <900000000>;
+};
+
+&pcie {
+	status = "okay";
+};
+
+&ethernet {
+	mtd-mac-address = <&factory 0xe000>;
+};
+
+&pinctrl {
+	state_default: pinctrl0 {
+		gpio {
+			ralink,group = "uart3";
+			ralink,function = "gpio";
+		};
+	};
+};
+

target/linux/ramips/image/mt7621.mk
@@ -83,6 +83,14 @@ define Device/gb-pc1
 endef
 TARGET_DEVICES += gb-pc1
 
+define Device/gb-pc2
+  DTS := GB-PC2
+  DEVICE_TITLE := GnuBee Personal Cloud Two
+  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3 kmod-sdhci-mt7620
+  IMAGE_SIZE := $(ralink_default_fw_size_32M)
+endef
+TARGET_DEVICES += gb-pc2
+
 define Device/hc5962
   DTS := HC5962
   BLOCKSIZE := 128k

commit 9a639ee1656cc980a25faed9cb8788c18b3f8f0b

ramips: improve GnuBee Personal Cloud Two support
Use the generic board detection for the GnuBee Personal Cloud Two
instead of the target specific one as all recent additions are doing.

Fixup the pinmux to set all pins used as GPIO to the function GPIO.
Request pins where used.

Drop the i2c from the dts. There is nothing connected. While at it fix an
indentation issue and use references instead of duplicating the whole
node path.

Use the same switch config as for the GB-PC1 and drop the led trigger for
the not supported IP1001 phy connected to second rgmii.

Fixes: c60a21532bc9 ("ramips: Add support for the GnuBee Personal Cloud Two")
Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/ramips/base-files/etc/board.d/01_leds
@@ -187,15 +187,10 @@ fonera20n)
 	set_usb_led "$boardname:orange:usb"
 	set_wifi_led "$boardname:orange:wifi"
 	;;
-gb-pc1)
+gb-pc1|\
+gnubee,gb-pc2)
 	ucidef_set_led_switch "lan1" "lan1" "$boardname:green:lan1" "switch0" "0x01"
 	ucidef_set_led_switch "lan2" "lan2" "$boardname:green:lan2" "switch0" "0x10"
-	;;
-gb-pc2)
-	ucidef_set_led_switch "lan" "lan" "$board:green:lan" "switch0" "0x01"
-	ucidef_set_led_switch "wan" "wan" "$board:green:wan" "switch0" "0x10"
-	ucidef_set_led_netdev "eth1-g" "eth1-g" "$board:green:eth1-g" "eth1"
-	ucidef_set_led_netdev "eth1-y" "eth1-y" "$board:green:eth1-y" "eth1"
 	;;
 gl-mt300a|\
 gl-mt300n|\

target/linux/ramips/base-files/etc/board.d/02_network
@@ -202,13 +202,10 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"1:lan:4" "2:lan:3" "3:lan:2" "4:lan:1" "0:wan" "6@eth0"
 		;;
-	gb-pc1)
+	gb-pc1|\
+	gnubee,gb-pc2)
 		ucidef_add_switch "switch0" \
 			"0:lan" "4:lan" "6@eth0"
-		;;
-	gb-pc2)
-		ucidef_add_switch "switch0" \
-			"4:lan" "0:wan" "6@eth0"
 		;;
 	gl-mt300n-v2)
 		ucidef_add_switch "switch0" \

target/linux/ramips/base-files/etc/diag.sh
@@ -91,7 +91,7 @@ get_status_led() {
 	dir-620-d1|\
 	dwr-512-b|\
 	gb-pc1|\
-	gb-pc2|\
+	gnubee,gb-pc2|\
 	hpm|\
 	hw550-3g|\
 	mac1200rv2|\

target/linux/ramips/base-files/lib/ramips.sh
@@ -205,9 +205,6 @@ ramips_board_detect() {
 	*"GB-PC1")
 		name="gb-pc1"
 		;;
-	*"GB-PC2")
-		name="gb-pc2"
-		;;
 	*"GL-MT300A")
 		name="gl-mt300a"
 		;;

target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -61,7 +61,7 @@ platform_check_image() {
 	fonera20n|\
 	freestation5|\
 	gb-pc1|\
-	gb-pc2|\
+	gnubee,gb-pc2|\
 	gl-mt300a|\
 	gl-mt300n|\
 	gl-mt750|\

target/linux/ramips/dts/GB-PC2.dts
@@ -18,12 +18,6 @@
 		bootargs = "console=ttyS0,57600";
 	};
 
-	palmbus: palmbus@1E000000 {
-		i2c@900 {
-			status = "okay";
-		};
-	};
-
 	gpio-keys-polled {
 		compatible = "gpio-keys-polled";
 		#address-cells = <1>;
@@ -50,23 +44,23 @@
 			gpios = <&gpio0 8 GPIO_ACTIVE_LOW>;
 		};
 
-		lan {
-			label = "gb-pc2:green:lan";
+		lan1 {
+			label = "gb-pc2:green:lan1";
 			gpios = <&gpio0 24 GPIO_ACTIVE_LOW>;
 		};
 
-		wan {
-			label = "gb-pc2:green:wan";
+		lan2 {
+			label = "gb-pc2:green:lan2";
 			gpios = <&gpio0 25 GPIO_ACTIVE_LOW>;
 		};
 
-		eth1-y {
-			label = "gb-pc2:yellow:eth1-y";
+		lan3-yellow {
+			label = "gb-pc2:yellow:lan3";
 			gpios = <&gpio0 13 GPIO_ACTIVE_LOW>;
 		};
 
-		eth1-g {
-			label = "gb-pc2:green:eth1-g";
+		lan3-green {
+			label = "gb-pc2:green:lan3";
 			gpios = <&gpio0 15 GPIO_ACTIVE_LOW>;
 		};
 
@@ -75,6 +69,9 @@
 
 &sdhci {
 	status = "okay";
+
+	pinctrl-names = "default";
+	pinctrl-0 = <&sdhci_pins>;
 };
 
 &spi0 {
@@ -114,8 +111,8 @@
 };
 
 &cpuclock {
-			compatible = "fixed-clock";
-			clock-frequency = <900000000>;
+	compatible = "fixed-clock";
+	clock-frequency = <900000000>;
 };
 
 &pcie {
@@ -129,7 +126,7 @@
 &pinctrl {
 	state_default: pinctrl0 {
 		gpio {
-			ralink,group = "uart3";
+			ralink,group = "jtag", "rgmii3", "uart3", "wdt";
 			ralink,function = "gpio";
 		};
 	};

target/linux/ramips/image/mt7621.mk
@@ -83,13 +83,13 @@ define Device/gb-pc1
 endef
 TARGET_DEVICES += gb-pc1
 
-define Device/gb-pc2
+define Device/gnubee_gb-pc2
   DTS := GB-PC2
   DEVICE_TITLE := GnuBee Personal Cloud Two
   DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3 kmod-sdhci-mt7620
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
 endef
-TARGET_DEVICES += gb-pc2
+TARGET_DEVICES += gnubee_gb-pc2
 
 define Device/hc5962
   DTS := HC5962