8dd3a74c6ee0a9a8ac3b2bca479cd5fbde387bdb
aluamorim <amorim.an@gmail.com>
2018-05-12 19:49:40 -0300

    new repo structure

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,31 +1,37 @@
 #!/bin/sh
 
-#kversion="4.14.37"
+#kversion="4.14"
+
 kversion="4.9.77"
 ip_addr="192.168.1.1"
-path="../openwrt"
+path="../lede/kernel-4.9"
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Missing parameters..."
-    echo "Usage:  #>copy.sh path_to_openwrt linkit_ip_addr"
+    echo "Usage:  #>copy.sh path_to_openwrt kernel_version linkit_ip_addr"
     exit
 else
 	path=$1
-	ip_addr=$2
+	kversion=$2
+	ip_addr=$3
 fi 
 
 #kversion="4.9.77"
-echo "Copying .ko files from: $1 ... "
+echo "Copying .ko files from: $path ... "
 
-cp $path/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion/sound/soc/mtk/*.ko ../linkit-files/
-cp $path/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion/sound/soc/ralink/*.ko ../linkit-files/
-#cp firmware/openwrt/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-4.9.77/sound/soc/codecs/*.ko ./
+cp $path/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion/sound/soc/mtk/*.ko ../linkit-files/modules/
+cp $path/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion/sound/soc/ralink/*.ko ../linkit-files/modules/
 
-echo "Sending files to linkit at IP=$2..."
-scp ../linkit-files/*.ko root@$ip_addr:/root
+echo "Copying image file ..."
+cp $path/bin/targets/ramips/mt76x8/*.bin ../linkit-files/lks7688.img
+
+
+echo "Sending files to linkit at IP=$ip_addr ..."
+scp ../linkit-files/modules/*.ko root@$ip_addr:/root
 scp ../linkit-files/run.sh root@$ip_addr:/root
-scp ../linkit-files/accounts root@$ip_addr:/root/.baresip/
-scp ../linkit-files/config root@$ip_addr:/root/.baresip/
+scp ../linkit-files/install_baresip_dep.sh root@$ip_addr:/root
+scp -pr ../linkit-files/accounts root@$ip_addr:/root/.baresip/
+scp -pr ../linkit-files/config root@$ip_addr:/root/.baresip/
 scp ../linkit-files/.asoundrc root@$ip_addr:/root/
 scp ../linkit-files/stereo.wav root@192.168.1.1:/root
 #scp male.wav root@192.168.1.1:/root
--- scripts/patch.sh
+++ scripts/patch.sh
@@ -3,17 +3,23 @@
 # Ele deve alterar o código baixado em "kpath", substituindo
 # os arquivos do porting de áudio.
 
+#kernel version
 #kversion="4.14.37"
 kversion="4.9.77"
-spath="../openwrt"
-kpath="$spath/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion"
-ppath="../porting/kernel-$kversion"
+#code path
+cpath="../lede/kernel-4.9"
 
-
+# user can specitfy code path and version
 if [ "$#" -eq 2 ]; then
-	spath=$1
+	cpath=$1
 	kversion=$2
 fi 
+
+#kernel path inside code path
+kpath="$cpath/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-$kversion"
+
+#porting path
+ppath="../porting/kernel-$kversion"
 
 rm -rf "$kpath/sound"
 rm -rf "$kpath/include/sound"
@@ -23,8 +29,11 @@ cp -R "$ppath/linux-$kversion/sound/" "$kpath/"
 cp -R "$ppath/linux-$kversion/include/sound/" "$kpath/include/"
 cp "$ppath/linux-$kversion/include/linux/interrupt.h" "$kpath/include/linux/"
 cp "$ppath/linux-$kversion/include/trace/events/asoc.h" "$kpath/include/trace/events/"
-#cp "$ppath/linux-$kversion/include/linux/wait.h" "$kpath/include/linux/"
-#cp "$ppath/package/kernel/linux/modules/sound.mk" "$spath/package/kernel/linux/modules/"
-#cp -R "$ppath/feeds/packages/libs/alsa-lib/" "$spath/feeds/packages/libs/"
-#cp -R "$ppath/feeds/packages/sound/alsa-utils/" "$spath/feeds/packages/sound/"
+
+if [ "$kversion" == "4.14.37" ]; then
+cp "$ppath/linux-$kversion/include/linux/wait.h" "$kpath/include/linux/"
+cp "$ppath/package/kernel/linux/modules/sound.mk" "$cpath/package/kernel/linux/modules/"
+cp -R "$ppath/feeds/packages/libs/alsa-lib/" "$cpath/feeds/packages/libs/"
+cp -R "$ppath/feeds/packages/sound/alsa-utils/" "$cpath/feeds/packages/sound/"	
+fi
 
