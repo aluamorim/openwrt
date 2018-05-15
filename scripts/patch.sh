f0e63830c8bae11de3180abba865a5e4eea3bf02
aluamorim <amorim.an@gmail.com>
2018-05-15 01:06:40 -0300

    sth strange

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -25,7 +25,7 @@ rm -rf "$kpath/sound"
 rm -rf "$kpath/include/sound"
 
 rm -rf "$ppath/package/base-files"
-tar -xvf "$ppath/base-files/" -C "$ppath/package"
+tar -xvf "$ppath/base-files.tar.xz" -C "$ppath/package"
 cp -R "$ppath/base-files/" "$cpath/package/"
 cp -R "$ppath/linux-$kversion/sound/" "$kpath/"
 cp -R "$ppath/linux-$kversion/include/sound/" "$kpath/include/"
